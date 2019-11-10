632cbd5f48d177972d7682a88a5cc01b57e4d1a6
francois <francois@macbook-pro-de-systeme-2.home>
2019-11-10 17:36:55 +0100

    england ba

---
+++ R/england_ba.R
@@ -0,0 +1,16 @@
+#' Ancient human DNA samples from David Reich's lab
+#' @description {The data include 143081 filtered SNP genotypes from 64 samples, from Great Britain (Bronze Age), Yamnaya culture and
+#' and from early farmers from Anatolia (Olalde et al. Nature, 2018). The data set is a list named 'England_BA'
+#'
+#' age: vector of ages in cal. years BP
+#'
+#' genotype: a 64x143081 numeric matrix containing genotypes corrected for coverage. The values
+#' correspond to the residuals of a latent factor regression model with log(coverage) considered as an explanatory variables.
+#'
+#' meta: metadata on individual ID, group ID, country of origin and coverage.
+#'}
+#' @name england_ba
+#' @docType data
+#'
+
+NULL
--- R/tfa.R
+++ R/tfa.R
@@ -12,7 +12,7 @@
 #' @param k an integer value for the number of factor to compute. The default value is k = 2.
 #' @param cov_matrix a user-specified nxn prior covariance matrix for the n samples. If \code{NULL},
 #' the prior matrix is computed as
-#' \code{C[i,j] = \min(t_i, t_j)},
+#' \code{C[i,j] = min(t_i, t_j)},
 #' where t_i and t_j are the normalized sampled dates, taking values between 0 and 1. The option is
 #' useful when the sample size is large, and when the user wants to avoid recomputing a same C
 #' matrix in every run.
@@ -39,31 +39,41 @@
 #' # Ancient DNA from Bronze Age Great Britain samples
 #' # including Steppe (Yamnaya) and early farmers from Anatolia
 #' data(england_ba)
-#' attach(england_ba)
-#' mod  = tfa(age,
-#'            geno_ancient,
+#' attach(England_BA)
+#' coverage <- as.numeric(as.character(meta$Coverage))
+#' mod  <- tfa(age,
+#'            genotype,
 #'            k = 2,
 #'            lambda = 7e-3,
 #'            center = TRUE,
-#'            coverage = meta$Coverage,
+#'            coverage = coverage,
 #'            log = TRUE)
 #'
 #' plot(mod$u, pch = 19, cex = 2, col = "grey90",
-#' xlab = "Factor 1", ylab = "Factor 2" )
+#'      xlab = "Factor 1", ylab = "Factor 2",
+#'      ylim = c(-20,20))
+#'
+#' legend(x = -55, y = -12,
+#'      c("Yamnaya","England_EBA","England_MBA   ","Anatolia"),
+#'      horiz = TRUE,
+#'      col = c("darkblue", "yellow3", "yellow4", "salmon3"),
+#'      pch = c(8,19,8,8), cex = .8, bty = "n")
 #'
 #' points(mod$u[meta$Country == "Great Britain",],
 #'        pch = 19, cex = 1, col = "yellow3")
-#' points(mod$w[meta$Country == "Russia",],
+#' points(mod$u[meta$Country == "Russia",],
 #'       pch = 8, cex = .6, col = "darkblue")
-#' points(mod$w[meta$Country == "Turkey",],
+#' points(mod$u[meta$Country == "Turkey",],
 #'       pch = 8, cex = .6, col = "salmon3")
-#' points(mod$w[meta$Group.ID == "England_MBA",],
+#' points(mod$u[meta$Group.ID == "England_MBA",],
 #'       pch = 8, cex = .6, col = "yellow4")
 #' abline(h = 0, lty = 2, col = "orange")
+#' legend()
 #'
-#' @references \url{https://www.biorxiv.org/content/10.1101/801324v3}
-#' François, O., Liégeois, S., Demaille, B., Jay, F. (2019). Inference of population genetic structure from temporal samples of DNA. bioRxiv, 801324.
-#' @seealso \code{\link{BA_England}}
+#' detach(England_BA)
+#' @references François, O., Liégeois, S., Demaille, B., Jay, F. (2019). Inference of population genetic structure from temporal samples
+#' of DNA. bioRxiv, 801324. \url{https://www.biorxiv.org/content/10.1101/801324v3}
+#' @seealso \code{\link{england_ba}}
 tfa <- function(sample_ages,
                 Y,
                 k = 2,
@@ -74,28 +84,29 @@ tfa <- function(sample_ages,
                 log = TRUE){
 
   n <- length(sample_ages)
-
+  if (!is.matrix(Y)) stop("Y must be a matrix.")
+  if (anyNA(Y)) stop("NA not allowed in the genetic data.")
+  if (anyNA(sample_ages)) stop("NA not allowed in the sample ages.")
+  if (dim(Y)[1] != n) stop("Number of Y rows (samples) not equal to the length of
+                           'sample_ages'.")
 
 
   # Covariance model
   # Default is Brownian covariance matrix
   if (is.null(cov_matrix)){
 
-  # conversion des ages en temps dans (0,1)
+  # conversion of ages as tn in (0,1)
   range_ages <- max(sample_ages) - min(sample_ages)
-  # error corrected here
-  tn <- 1 - (age_ancient - min(age_ancient))/range_ages
-
-  # normalisation des temps par la variance des lignes
-  # entre min et max de la variance
+  tn <- 1 - (sample_ages - min(sample_ages))/range_ages
+  # normalisation of times
   var_Y <- apply(Y, 1, FUN = var)
   tn <- min(var_Y) +  (max(var_Y) - min(var_Y)) *tn
 
-  # scale
+  # scale Y
   if (center) {
     Y = t(scale(t(Y), scale = FALSE, center = TRUE))
   }
-
+  # Brownian model for scaled data
   C <- matrix(NA, n, n)
   for (i in 1:n){
     for (j in 1:n)  C[i,j] <- min(tn[i], tn[j])}
@@ -125,21 +136,20 @@ tfa <- function(sample_ages,
   if (!is.null(coverage)){
     if (log){
       for (i in 1:k){
-        plot(log(coverage), Wn[,k], col = "grey", pch = 19, cex = .8)
-        mod = loess(Wn[,k]  ~ log(coverage))
-        points(log(coverage), fitted(mod), col = 2, pch = 19, cex = 1)
-        Wn[,k] <- Wn[,k] - fitted(mod)}
-      Un <-  prcomp(Wn)$x
+        #plot(log(coverage), Wn[,k], col = "grey", pch = 19, cex = .8)
+        mod = loess(Un[,k]  ~ log(coverage))
+        #points(log(coverage), fitted(mod), col = 2, pch = 19, cex = 1)
+        Un[,k] <- Un[,k] - fitted(mod)}
+      Un <-  prcomp(Un)$x
     } else {
       for (i in 1:k){
-        plot(coverage, Wn[,k], col = "grey", pch = 19, cex = .8)
-        mod = loess(Wn[,k]  ~ coverage)
-        points(coverage, fitted(mod), col = 2, pch = 19, cex = 1)
-        Wn[,k] <- Wn[,k] - fitted(mod)}
-      Un <-  prcomp(Wn)$x
+        #plot(coverage, Wn[,k], col = "grey", pch = 19, cex = .8)
+        mod = loess(Un[,k]  ~ coverage)
+        #points(coverage, fitted(mod), col = 2, pch = 19, cex = 1)
+        Un[,k] <- Un[,k] - fitted(mod)}
+      Un <-  prcomp(Un)$x
     }
   }
   # returns corrected factors in $u
   return(list(tn = tn, u = Un, cov = C))
 }
-
